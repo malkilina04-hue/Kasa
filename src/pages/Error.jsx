2c18056125874000eb73109b42ff05846f738c59
malkilina04-hue <malkilina04@gmail.com>
2026-05-27 14:40:53 +0200

    Etape 6 page error 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,4 +1,14 @@
+import { Link } from 'react-router-dom'
+import './Error.scss'
+
 function Error() {
-    return <h1>Page error 404 </h1>
+    return (
+        <div className="error">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/">Retourner sur la page d'accueil</Link>
+        </div>
+    )
 }
+
 export default Error
